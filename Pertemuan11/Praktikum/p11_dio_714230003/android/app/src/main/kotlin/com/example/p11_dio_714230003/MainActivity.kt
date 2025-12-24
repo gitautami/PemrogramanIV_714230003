7226b89d0cb295ef87deaecf12adfc3a896d3d76
gitautami <utamigita797@gmail.com>
2025-12-24 21:28:59 +0700

    upload tugas pertemuan11

---
+++ Pertemuan11/Praktikum/p11_dio_714230003/android/app/src/main/kotlin/com/example/p11_dio_714230003/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p11_dio_714230003
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Pertemuan11/Praktikum/p11_dio_714230003/lib/data_service.dart
@@ -0,0 +1,120 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'user.dart';
+
+class DataService {
+  final Dio dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://reqres.in/api',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'x-api-key':
+            'reqres_bbca121c38f748198e21e44314b34e2b',
+      },
+    ),
+  );
+
+  Future getUsers() async {
+    try {
+      final res = await dio.get('/users');
+
+      debugPrint('STATUS : ${res.statusCode}');
+      debugPrint('DATA : ${res.data}');
+      return res.data;
+    } catch (e) {
+      debugPrint('ERROR : $e');
+      return null;
+    }
+  }
+
+  Future<UserCreate?> postUser(UserCreate user) async {
+    try {
+      final response = await dio.post('/users', data: user.toMap());
+
+      debugPrint('STATUS: ${response.statusCode}');
+      debugPrint('DATA : ${response.data}');
+
+      if (response.statusCode == 201) {
+        return UserCreate.fromJson(response.data);
+      }
+
+      return null;
+    } on DioException catch (e) {
+      debugPrint('DIO ERROR STATUS: ${e.response?.statusCode}');
+      debugPrint('DIO ERROR DATA : ${e.response?.data}');
+      return null;
+    } catch (e) {
+      debugPrint('ERROR: $e');
+      return null;
+    }
+  }
+
+  Future<UserUpdate?> putUser(String idUser, String name, String job) async {
+    try {
+      final response = await dio.put(
+        '/users/$idUser',
+        data: {'name': name, 'job': job},
+      );
+
+      debugPrint('STATUS: ${response.statusCode}');
+      debugPrint('DATA : ${response.data}');
+
+      if (response.statusCode == 200) {
+        return UserUpdate.fromJson(response.data);
+      }
+      return null;
+    } on DioException catch (e) {
+      debugPrint('DIO ERROR STATUS: ${e.response?.statusCode}');
+      debugPrint('DIO ERROR DATA : ${e.response?.data}');
+      return null;
+    } catch (e) {
+      debugPrint('ERROR: $e');
+      return null;
+    }
+  }
+
+  Future deleteUser(String idUser) async {
+    try {
+      final response = await dio.delete('/users/$idUser');
+
+      debugPrint('STATUS: ${response.statusCode}');
+      debugPrint('DATA : ${response.data}');
+
+      if (response.statusCode == 204) {
+        return 'Delete user successful';
+      }
+
+      return null;
+    } on DioException catch (e) {
+      debugPrint('DIO ERROR STATUS: ${e.response?.statusCode}');
+      debugPrint('DIO ERROR DATA : ${e.response?.data}');
+      return null;
+    } catch (e) {
+      debugPrint('ERROR: $e');
+      return null;
+    }
+  }
+
+  Future<Iterable<User>?> getUserModel() async {
+    try {
+      var response = await dio.get('/users');
+
+      if (response.statusCode == 200) {
+        final users = (response.data['data'] as List)
+            .map((e) => User.fromJson(e))
+            .toList();
+
+        return users;
+      }
+      return null;
+    } on DioException catch (e) {
+      debugPrint('DIO ERROR STATUS: ${e.response?.statusCode}');
+      debugPrint('DIO ERROR DATA : ${e.response?.data}');
+      return null;
+    } catch (e) {
+      debugPrint('ERROR: $e');
+      return null;
+    }
+  }
+}
